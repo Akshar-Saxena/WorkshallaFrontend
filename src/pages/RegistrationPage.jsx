4de6874c7e77725d60ea5367bd489c5f4d9ed362
Akshar Saxena <aksharsaxena2003@gmail.com>
2023-11-13 18:32:42 +0530

    added hamburger functionality

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,39 +1,38 @@
 import React from 'react'
-import "./RegistrationPage.css"
-export default function RegistrationPage(){
-    return(
-        <>
-        
-      <div className="registration-container">
-      <div className="image-container">
-        <img src="../src/assets/regPageImage.png" alt="" />  
+import { Link } from 'react-router-dom';
+
+
+export default function RegistrationPage() {
+  return (
+      <div className="flex place-items-center justify-evenly h-[100vh] m-auto">
+        <div className="top-0 -z-10  w-[42%] min-[280px]:absolute opacity-70 min-[280px]:w-full min-[280px]:h-[100vh] md:static md:w-[48%]">
+          <img className='min-[280px]:w-full min-[280px]:h-full' src="../src/assets/regPageImage.png" alt="" />
         </div>
-       <div className="registration-form">
-          <h1>Register</h1>
-        
-          <div className="form-group">
+        <div className="w-[28%] min-[280px]:w-[80%] md:w-[28%]">
+          <h1 className='text-4xl font-bold pb-7'>Register</h1>
+
+          <div className="py-3 flex flex-col w-full">
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" placeholder='Enter your Name' />
+            <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black' type="text" id="name" placeholder='Enter your Name' />
           </div>
-          <div className="form-group">
+          <div className="py-3 flex flex-col w-full">
             <label htmlFor="phoneNumber">Phone Number</label>
-            <input type="tel" id="phoneNumber" placeholder='Enter your phone number'/>
+            <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black' type="tel" id="phoneNumber" placeholder='Enter your phone number' />
           </div>
-          <div className="form-group">
+          <div className="py-3 flex flex-col w-full">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder='Enter your Email' />
+            <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black' type="email" id="email" placeholder='Enter your Email' />
           </div>
-          <div className="form-group">
+          <div className="py-3 flex flex-col w-full">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder='Enter your password' />
+            <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black' type="password" id="password" placeholder='Enter your password' />
           </div>
-          <button type="submit">Sign Up</button>
-          <div className="belowButton">
-            <h3>Already Registered!</h3> <a href="#">Login</a>
+          <button className='py-2 px-5 bg-[#946CC3] text-white text-xs w-full rounded-md mt-10'>Sign Up</button>
+          <div className="flex justify-center mt-3">
+            <h3 className='font-bold'>Already Registered!</h3> <Link to="/login" className='text-[#946CC3]'>Login</Link>
+          </div>
         </div>
-        </div>
-    </div>
-</>
+      </div>
   );
 }
 
--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -40,18 +40,18 @@ export default function ResetPassword() {
             <img className='w-[45%] min-[280px]:absolute min-[280px]:-z-10 min-[280px]:opacity-70 min-[280px]:h-full min-[280px]:w-full md:static md:h-auto md:opacity-100 md:w-[60%] lg:h-auto lg:w-[45%]' src="../src/assets/resetPassword.png" alt="" />
             <div className='min-[280px]:w-full lg:w-[35%] flex flex-col justify-center place-items-center'>
                 <div className='min-[280px]:w-[80%]'>
-                    <h1 className='font-bold text-4xl pb-10 min-[280px]:text-2xl'>Reset Password</h1>
-                    <label className='mt-5 mb-1'>New Password</label>
-                    <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black' type="text" placeholder='Enter your New Password' />
-                    <label className='mt-5 mb-1'>Confirm Password</label>
-                    <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black' type="text" placeholder='Re-write your New Password' />
+                    <h1 className='font-bold md:text-4xl pb-10 min-[280px]:text-2xl'>Reset Password</h1>
+                    <label className=''>New Password</label>
+                    <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black mb-4' type="text" placeholder='Enter your New Password' />
+                    <label className=''>Confirm Password</label>
+                    <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black mb-4' type="text" placeholder='Re-write your New Password' />
                     <div onClick={togglerHandler} className='outline outline-1 outline-[#946CC3] h-fit w-fit flex my-5 rounded-md cursor-pointer'>
                         <div className={`min-[280px]: text-xs py-1 px-4 rounded-md mx-2 my-1 bg-[${emailToggleColors[0]}] text-${emailToggleColors[1]} transition-all`}>Email</div>
                         <div className={`min-[280px]: text-xs py-1 px-4 rounded-md mx-2 my-1 bg-[${phoneToggleColors[0]}] text-${phoneToggleColors[1]} transition-all`}>Phone Number</div>
                     </div>
-                    <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black' type="text" placeholder={placeholder} />
-                    <label className='mt-5 mb-1'>OTP</label>
-                    <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black' type="text" placeholder='Enter your OTP' />
+                    <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black mb-4' type="text" placeholder={placeholder} />
+                    <label className=''>OTP</label>
+                    <input className='text-xs h-[35px] w-full pl-3 rounded-md outline outline-1 outline-black mb-4' type="text" placeholder='Enter your OTP' />
                     <button className='py-2 px-5 bg-[#946CC3] text-white text-xs w-full rounded-md mt-10' onClick={buttonToggleHandler} style={{ display: `${otpButton}` }}>Send OTP</button>
                     <button className='py-2 px-5 bg-[#946CC3] text-white text-xs w-full rounded-md mt-10' onClick={resetButtonHandler} style={{ display: `${resetButton}` }}>Reset my Password</button>
                 </div>
