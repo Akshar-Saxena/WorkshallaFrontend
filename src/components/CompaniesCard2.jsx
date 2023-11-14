6243f3e8bd9562f9e6e47885f51aa0111c7156de
prkgr799 <prkgr799@gmail.com>
2023-11-15 01:34:02 +0530

    Companies Page made responsive

--- src/components/CompaniesCard2.jsx
+++ src/components/CompaniesCard2.jsx
@@ -26,7 +26,7 @@ export default function CompaniesCard2() {
       <button className='bg-purple-700 mt-10 text-white py-2 px-12 rounded-lg hover:bg-purple-900 min-[280px]:px-2 min-[280px]:text-xs'>View Jobs</button>
     </div>
 
-    <div className='pt-10 rounded-lg outline outline-purple-600 outline-1 ml-1 mr-8 pb-5 flex flex-col justify-center place-items-center'>
+    <div className='pt-10 rounded-lg outline outline-purple-600 outline-1 ml-1 mr-8 pb-5 flex flex-col justify-center place-items-center min-[280px]:mt-2'>
       <img className='w-14' src="assets/microsoftLogo.png" alt="" />
       <div className='rounded-lg px-6 py-2 my-8 mx-5 bg-pink-200'>
         <h3 className="ml-6 min-[280px]:text-xs ">Microsoft</h3>
@@ -38,7 +38,7 @@ export default function CompaniesCard2() {
     </div>
 
 
-    <div className='pt-10 rounded-lg outline outline-purple-600 outline-1 ml-1 mr-8 pb-5 flex flex-col justify-center place-items-center'>
+    <div className='pt-10 rounded-lg outline outline-purple-600 outline-1 ml-1 mr-8 pb-5 flex flex-col justify-center place-items-center min-[280px]:mt-2'>
       <img className='w-14' src="assets/appleLogo.png" alt="" />
       <div className='rounded-lg px-6 py-2 my-8 mx-5 bg-pink-200'>
         <h3 className="ml-10 min-[280px]:text-xs">Apple</h3>
