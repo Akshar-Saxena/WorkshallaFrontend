7da1a5caa082ba5197c2e9a394747fed6ab11139
Akshar Saxena <aksharsaxena2003@gmail.com>
2023-12-19 16:50:51 +0530

    companies api error

--- src/pages/CompaniesPage.jsx
+++ src/pages/CompaniesPage.jsx
@@ -43,6 +43,7 @@ export default function CompaniesPage() {
 
     useEffect(() => {
         window.scrollTo(0, 0);
+        console.log("Func Calling");
         return () => getData();
     }, []);
 
